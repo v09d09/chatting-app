ac5ac727f2d7badce7bfafad24dcf9e3a88ecb63
v09d09 <mohamedheekal@gmail.com>
2022-01-09 20:34:05 +0200

    added layout

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import ChatBox from "./components/ChatBox";
+
+function App() {
+  return (
+    <div className="flex h-screen ">
+      <div className=" w-80"></div>
+      <ChatBox />
+      <div className=" w-80"></div>
+    </div>
+  );
+}
+
+export default App;
